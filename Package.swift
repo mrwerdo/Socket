2c7b161d283f7982a6936e10723c54660292e74d
Andrew Thompson <mrwerdo331@me.com>
2016-01-06 09:59:52 +1100

    Fixed linking errors encountered when build sockdev

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@ import PackageDescription
 // Package information stuff.
 
 let libraryTarget = Target(name: "SocketLib")
-let sockdev = Target(name: "sockdev", dependencies: [.Target(name: "SocketLib")])
+let dev = Target(name: "dev", dependencies: [.Target(name: "SocketLib")])
 
-let package = Package(name: "Socket", targets: [libraryTarget, sockdev])
+let package = Package(name: "Socket", targets: [libraryTarget, dev])
 
