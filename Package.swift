d941fead7bf09387cdb3cd89307eda73cd4999f9
Andrew Thompson <mrwerdo331@me.com>
2016-01-15 20:15:43 +1100

    Reconfigured package description

--- Package.swift
+++ Package.swift
@@ -3,17 +3,17 @@ import Darwin
 
 var targets: [Target] = []
 /// The public interface to the library
-targets.append(Target(name: "Library"))
+targets.append(Target(name: "SocketLib"))
 
 var buffer = [CChar](count: 1024, repeatedValue: 0)
 if let workingDir = String.fromCString(getcwd(&buffer, 1024))
 {
     if access ("./Sources/dev/main.swift", F_OK) != -1 {
         /// My developer environment - it's private
-        let dev = Target(name: "dev", dependencies: [.Target(name: "Library")])
+        let dev = Target(name: "dev", dependencies: [.Target(name: "SocketLib")])
         targets.append(dev)
     }
 }
 
-let package = Package(name: "SocketLib", targets: targets)
+let package = Package(name: "Socket", targets: targets)
 
