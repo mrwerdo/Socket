e928f00934698c908555f76f29a681ccfb005f77
Andrew Thompson <mrwerdo331@me.com>
2020-04-24 17:17:29 +1000

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,14 @@
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "Socket",
-    targets: [ // ]
-        Target(name: "Support"),
-        Target(name: "Socket", dependencies: ["Support"])
+    products: [
+        .library(name: "Socket", targets: ["Socket"])
+    ],
+    targets: [
+        .target(name: "Support"),
+        .target(name: "Socket", dependencies: ["Support"]),
+        .testTarget(name: "SocketTests", dependencies: ["Socket"])
     ]
 )
